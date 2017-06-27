3cf51620796537de33bd1ff0ee7b598e50d0aca5
Miles McCain <milesmcc@users.noreply.github.com>
2017-06-27 00:52:05 -0400

    Added proper URL checking

--- flashes.py
+++ flashes.py
@@ -8,6 +8,7 @@ import tweepy
 import tweepy
 import re
 from threading import Thread
+import json
 
 latest_flashes = []
 link_regex = re.compile(r"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»\"\"'']))")
@@ -47,10 +48,11 @@ def load_flashes():
         say("Loading flashes from Twitter account " + accountPair[0] + " (" + accountPair[1] + ")")
         latest_tweets = twitter.get_latest_statuses(accountPair[0][1:])
         for tweet in latest_tweets:
-            print tweet
             url = None
-            if "urls" in tweet:
-                url = tweet["urls"][0]['expanded_url']
+            if "entities" in tweet:
+                if "urls" in tweet['entities']:
+                    if len(tweet['entities']['urls']) > 0:
+                        url = tweet['entities']["urls"][0]['expanded_url']
             push_flash(
                 generate_flash(tweet["text"], url, accountPair[1], tweet["id"], tweet["created_at"])
                 )
