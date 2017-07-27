3ff55eda687eacff5b936f9db3f60d07fb21163c
Miles McCain <milesmcc@users.noreply.github.com>
2017-07-26 23:04:26 -0400

    Stats basics (not prod ready, obv)

---
+++ src/stats.py
@@ -0,0 +1,27 @@
+from geoip import geolite2
+from datetime import datetime
+import pytz
+import humanize
+
+requests = 0
+ips = []
+starttime = datetime.now(pytz.UTC)
+
+def request(ip_string):
+    requests += 1
+    if ip_string not in ips:
+        ips.append(ip_string)
+    match = geolite2.lookup(ip_string)
+    if match:
+        return match.continent + "/" + match.country + " - " + ip_string
+    else:
+        return "? - " + ip_string
+
+def unique_devices(): # give or take
+    return len(ips)
+
+def time():
+    return humanize.naturaltime(datetime.now(pytz.UTC) - starttime)
+
+def requests_per_second():
+    return (requests + 0.0) / (datetime.now(pytz.UTC) - starttime).total_seconds()
