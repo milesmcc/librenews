4a5ea803f8de91bcf895c93a88c813fbdf011c38
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2018-01-12 12:43:28 -0500

    Brought code into PEP8 compliance

--- src/twitter.py
+++ src/twitter.py
@@ -1,6 +1,6 @@
-import tweepy
 import configuration
-from userio import *
+import tweepy
+from userio import say
 
 twitter_credentials = configuration.get_twitter_credentials()
 
@@ -12,10 +12,13 @@ auth.set_access_token(twitter_credentials["access_token"],
 api = tweepy.API(auth)
 
 id_cache = {}
+
+
 def get_id(username):
     if username not in id_cache:
         id_cache[username] = api.get_user(username).id
     return id_cache[username]
+
 
 def get_latest_statuses(username):
     say("Gathering latest statuses from " + username)
